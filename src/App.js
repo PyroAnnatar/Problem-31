fb4f5dd5e79ad91b4a58cb266a456363fa79ecd0
PyroAnnatar <pyroannatar@gmail.com>
2024-09-07 19:51:30 +0300

    Fun

--- src/App.js
+++ src/App.js
@@ -1,38 +1,50 @@
-import Image from 'next/image'
-import { createContext, useContext } from 'react'
+import Image from "next/image";
+import { createContext, useContext } from "react";
+
+const SaleContext = createContext();
 
 export default function App() {
-  return <Hero />
+  return (
+    <SaleProvider>
+      <Hero />
+    </SaleProvider>
+  );
+}
+
+function SaleProvider({ children }) {
+  const value = { discount: "%50", placeholder: "Yeni indirimler yolda" };
+
+  return <SaleContext.Provider value={value}>{children}</SaleContext.Provider>;
 }
 
 function Hero() {
-  const value = useContext(SaleContext)
+  const { discount, placeholder } = useContext(SaleContext);
 
   return (
-    <div className='relative bg-gray-900'>
-      <div aria-hidden='true' className='absolute inset-0 overflow-hidden'>
+    <div className="relative bg-gray-900">
+      <div aria-hidden="true" className="absolute inset-0 overflow-hidden">
         <Image
-          src='/hero-full-width.jpg'
-          alt='Office Supplies'
-          className='h-full w-full object-cover object-center'
+          src="/hero-full-width.jpg"
+          alt="Office Supplies"
+          className="h-full w-full object-cover object-center"
           width={500}
           height={500}
         />
       </div>
-      <div className='relative mx-auto flex max-w-3xl flex-col items-center px-6 py-32 text-center sm:py-64 lg:px-0'>
-        <h1 className='text-5xl font-bold tracking-tight text-white'>
-          Her şey X indirimli olsun
+      <div className="relative mx-auto flex max-w-3xl flex-col items-center px-6 py-32 text-center sm:py-64 lg:px-0">
+        <h1 className="text-5xl font-bold tracking-tight text-white">
+          {discount ? `Her şey ${discount} indirimli olsun` : placeholder}
         </h1>
-        <p className='mt-4 text-xl text-white'>
+        <p className="mt-4 text-xl text-white">
           Yaz sürümümüzden seçenekler hala varken, en son gelişmelere göz atın
         </p>
         <a
-          href='#'
-          className='mt-8 inline-block rounded-md border border-transparent bg-white px-8 py-3 text-base font-medium text-gray-900 hover:bg-gray-100'
+          href="#"
+          className="mt-8 inline-block rounded-md border border-transparent bg-white px-8 py-3 text-base font-medium text-gray-900 hover:bg-gray-100"
         >
           Yeni Gelenleri Satın Alın
         </a>
       </div>
     </div>
-  )
+  );
 }
